f8cdd0882009b0967a4181693ca36952e1d58743
Elkin Botero <147097939+eybagit@users.noreply.github.com>
2025-06-19 01:33:21 +0000

    Generador de excusas refactorizado

--- src/app.js
+++ src/app.js
@@ -1,8 +1,3 @@
-// Definición de arrays con partes de la excusa
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird', 'Bobby'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'my phone', 'the car'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
 
 function getRandom(anyArray) {
@@ -11,6 +6,12 @@ function getRandom(anyArray) {
 
 // Al cargar la ventana, se selecciona el elemento con ID "excuse" y se le asigna una excusa aleatoria
 window.onload = function () {
+  // Definición de arrays con partes de la excusa
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird', 'Bobby'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
   const excuseElement = document.querySelector('#excuse');
   console.log("Hello Rigo from the console!");
   excuseElement.innerHTML = `<h1>${getRandom(who)} ${getRandom(action)} ${getRandom(what)} ${getRandom(when)}</h1>`;
