11148167b6256f05613c3ba70b8891f7b3c0fdd7
Elkin Botero <147097939+eybagit@users.noreply.github.com>
2025-06-19 01:21:25 +0000

    refactorizacion de funcion

--- src/app.js
+++ src/app.js
@@ -4,15 +4,9 @@ let action = ['ate', 'peed', 'crushed', 'broke'];
 let what = ['my homework', 'my phone', 'the car'];
 let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-// Función para obtener un número aleatorio entre "a" (incluido) y "b" (excluido)
-function getRandomNumber(a, b) {
-  return Math.floor(Math.random() * (b - a) + a);
-}
 
-// Función que devuelve un elemento aleatorio de un array
 function getRandom(anyArray) {
-  let max = anyArray.length;
-  return anyArray[getRandomNumber(0, max)];
+  return anyArray[Math.floor(Math.random() * anyArray.length)];
 }
 
 // Al cargar la ventana, se selecciona el elemento con ID "excuse" y se le asigna una excusa aleatoria
